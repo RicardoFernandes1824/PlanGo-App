c945587967b3700cd2899b4f4ed5cb17f758871a
Ricardo Fernandes <ricardo.fernandes@ipvc.pt>
2025-01-20 23:42:42 +0000

    update

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,22 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {
-  IonButton,
-  IonButtons,
-  IonContent,
-  IonDatetime,
-  IonDatetimeButton,
-  IonHeader,
-  IonInput,
-  IonItem,
-  IonLabel,
-  IonModal,
-  IonSelect,
-  IonSelectOption,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/angular/standalone';
-import {ModalController, ToastController} from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { IonButton, IonButtons, IonContent, IonDatetime, IonDatetimeButton, IonHeader, IonInput, IonItem, IonLabel, IonModal, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -24,20 +11,8 @@ import {ModalController, ToastController} from '@ionic/angular';
   styleUrls: ['./modal.component.scss'],
   imports: [
     FormsModule,
-    IonButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonInput,
-    IonItem,
-    IonTitle,
-    IonToolbar,
-    IonSelectOption,
-    IonSelect,
-    IonDatetime,
-    IonDatetimeButton,
-    IonModal,
-    IonLabel,
+    CommonModule,
+    IonicModule,
   ],
   standalone: true,
 })
@@ -47,9 +22,40 @@ export class ModalComponent {
   @Input() tripState: string = '';
   @Input() tripStart: string = (new Date()).toISOString();
   @Input() tripEnd: string = (new Date()).toISOString();
+  @Input() locations: any[] = []; // Array to store locations
 
-  constructor(private modalCtrl: ModalController, private toastController: ToastController) { }
+  locationDescription: string = '';
+  locationType: string = '';
+  locationState: string = '';
+  locationStart: string = (new Date()).toISOString();
+  locationEnd: string = (new Date()).toISOString();
 
+  constructor(private modalCtrl: ModalController, private toastController: ToastController) {}
+
+  addLocation() {
+    if (this.locationStart >= this.locationEnd) {
+      this.presentToast('The start date of the location cannot be after the end date', 'danger');
+      return;
+    }
+
+    const location = {
+      description: this.locationDescription,
+      type: this.locationType,
+      state: this.locationState,
+      start: this.locationStart,
+      end: this.locationEnd,
+    };
+    this.locations.push(location);
+
+    // Clear the input fields after adding the location
+    this.locationDescription = '';
+    this.locationType = '';
+    this.locationState = '';
+    this.locationStart = (new Date()).toISOString();
+    this.locationEnd = (new Date()).toISOString();
+
+    this.presentToast('Location added successfully');
+  }
 
   async presentToast(message: string, color: string = 'success') {
     const toast = await this.toastController.create({
@@ -67,16 +73,19 @@ export class ModalComponent {
 
   async confirm() {
     if (new Date(this.tripStart) >= new Date(this.tripEnd)) {
-      await this.presentToast('The start date can\'t be after the end date', 'danger')
+      await this.presentToast('The trip start date cant be after the trip end date', 'danger');
       return;
     }
+
     const tripData = {
       tripDescription: this.tripDescription,
       tripType: this.tripType,
       tripState: this.tripState,
       tripStart: this.tripStart,
       tripEnd: this.tripEnd,
+      locations: this.locations,
     };
+
     return this.modalCtrl.dismiss(tripData, 'confirm');
   }
 }
